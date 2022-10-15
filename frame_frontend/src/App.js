7462040ff0eeae7e888f61132dc0e1ca37dd44fe
Amandeep <singh.amandeep5665@gmail.com>
2022-10-14 18:55:26 -0700

    setting up routes and starting login page

--- frame_frontend/src/App.js
+++ frame_frontend/src/App.js
@@ -1,10 +1,13 @@
-
+import {Routes,Route,useNavigate} from 'react-router-dom'
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
   return (
-    <div className="App">
-      hrhr
-    </div>
+    <Routes>
+      <Route path='login' element={<Login />}></Route>
+      <Route path='/*' element={<Home />}></Route>
+      </Routes >
   );
 }
 
